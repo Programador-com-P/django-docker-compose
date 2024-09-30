0304b1614a745583f48833ee1e08407a7698c64e
Carlos Daniel Gomes Santana <cdgomessantana@example.com>
2024-09-29 22:30:39 -0300

    Modified a name of django project

--- django_project/src/settings.py
+++ django_project/src/settings.py
@@ -29,7 +29,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_project.urls'
+ROOT_URLCONF = 'src.urls'
 
 TEMPLATES = [
     {
@@ -47,7 +47,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_project.wsgi.application'
+WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,7 @@ python manage.py migrate --no-input
 # Coletar arquivos estáticos
 python manage.py collectstatic --no-input
 
+DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username=$SUPER_USER_NAME --email=$SUPER_USER_EMAIL --noinput
+
 # Iniciar o Gunicorn
-gunicorn django_project.wsgi:application --bind 0.0.0.0:8000
+gunicorn src.wsgi:application --bind 0.0.0.0:8000
